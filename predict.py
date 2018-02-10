77ede7a9c09f87441f4f3ad07c96788856f2d865
myoneda <hydrangea966@gmail.com>
2018-02-10 16:38:34 +0900

    説明

--- predict.py
+++ predict.py
@@ -17,6 +17,12 @@ def reshape(digits):
 
 
 def fit(X_train, y_train, score):
+    # score_wighted
+
+    # 交差検証を行う
+    # cv=5 ５分割する
+    # precision 適合率を重視する
+    # C=1 誤った分類をどの程度許容するか
     clf = GridSearchCV(SVC(C=1), get_parmeters(), cv=5,
                        scoring='%s_weighted' % score)
     clf.fit(X_train, y_train)
@@ -25,20 +31,27 @@ def fit(X_train, y_train, score):
 
 
 def main():
+    # データ読み込み
     digits = datasets.load_digits()
+    # imagesの8x8のarrayを1次元のarrayに変更
+    # sklearnに読み込ませるため。8x8も1x64も同じ
     X = reshape(digits)
     y = digits.target
 
+    # テストデータと検証データに分割
+    # テストサイズは半分
+    # random stateを設定することで、何度やっても同じ分割が得られるようにする
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.5,
         random_state=0,
     )
 
     # 適合率で最適化
+    # 100件のうち、正解が60だったら60÷100で0.6
     score = 'precision'
     clf = fit(X_train, y_train, score)
     y_true, y_pred = y_test, clf.predict(X_test)
-
+    # 混同行列を表示
     predict_results = confusion_matrix(y_true, y_pred)
     print 'predict test data'
     print predict_results
